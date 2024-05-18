4cb2e32973ccd92252494f291ebe9fd5e304fd26
Shane OHanlon <sohanlon07@gmail.com>
2024-05-18 20:32:51 +0100

    feat: add comic rename script and readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# useful-scripts
-Some Useful Scripts
+# My Useful Scripts
+Some Useful Scripts to do stuff I can't be bothered doing manually. See comments in scripts for info on what they do.
---
+++ file-ops/comics-files-bulk-rename.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Renames comic files downloaded from a specifc source that use a specific format.
+# Removes extra data from file name; for example: 
+# Batman - The Brave and the Bold 012 (2024) (Webrip) (The Last Kryptonian-DCP).cbr becomes
+# Batman - The Brave and the Bold 012.cbr
+# Should be used only on backups of comics you have purchased.
+
+# Loop through all files in the current directory
+for filename in *; do
+  # Check if the filename contains a dot (likely indicating an extension)
+  if [[ $filename =~ \. ]]; then
+    # Extract the filename without the extension
+    base_name="${filename%.*}"
+    # Extract the extension
+    extension="${filename##*.}"
+    
+    # Remove everything from the first bracket ([) to the end
+    new_name="${base_name%%(*)}"
+    
+    # Rebuild the filename with the new name and extension, removing space
+    new_filename="$new_name.$extension"
+    
+    # Rename the file only if the new name is different
+    if [[ "$filename" != "$new_filename" ]]; then
+      mv "$filename" "$new_filename"
+      echo "Renamed: $filename -> $new_filename"
+    fi
+  fi
+done
+
+echo "Finished renaming files."
